5e7a51e779b2d5d3a7d1c99d703aa78aee80fed4
“TJX” <juanxitian1031@gmail.com>
2024-07-14 19:41:42 +0800

    update

--- Optimizers/adam_mini.py
+++ Optimizers/adam_mini.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import torch
 from torch.optim.optimizer import Optimizer
 import math
@@ -9,6 +8,7 @@ from torch.optim.optimizer import _dispatch_sqrt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 from torch.distributed._tensor import Replicate
+
 
 class Adam_mini(Optimizer):
     def __init__(
@@ -23,6 +23,18 @@ class Adam_mini(Optimizer):
             n_head=32,
             n_kv_head=None,
     ):
+        '''
+        model: the model you are training.
+
+        model_sharding: set to True if you are using model parallelism with more than 1 GPU, including FSDP and zero_1,2,3 in Deepspeed. Set to False if otherwise.
+
+        n_feature: dimension for hidden feature.  Could be unspecified if you are training non-transformer models.
+
+        n_head: number of attention heads. Could be unspecified if you are training non-transformer models.
+
+        n_kv_head: number of head for Key and Value. Or equivalently, number of query groups in Group query Attention. Also known as "n_query_groups".  If not specified, it will be equal to n_head. Could be unspecified if you are training non-transformer models.
+        '''
+
         self.n_feature = n_feature
         self.n_head = n_head
         if n_kv_head is not None:
@@ -39,57 +51,74 @@ class Adam_mini(Optimizer):
         optim_groups = []
         count_embd = 0
         count_output = 0
-        count_qk = 0
+        count_q = 0
+        count_k = 0
         for name, param in self.model.named_parameters():
+            print('Detected param blocks by Adam-mini:', name, param.size())
             if param.requires_grad:
                 dic = {}
                 dic["name"] = name
                 dic["params"] = param
-                if "norm" in name or "ln_f" in name:
+                if ("norm" in name or "ln_f" in name):
                     dic["weight_decay"] = 0
                 else:
                     dic["weight_decay"] = weight_decay
 
-                if "embed_tokens" in name or "wte" in name or "tok_embeddings" in name:
+                if ("embed" in name or "wte" in name or "embd" in name):
                     count_embd += 1
 
-                if "lm_head" in name or "output.weight" in name:
+                if ("lm_head.weight" in name or "output.weight" in name):
                     count_output += 1
 
-                if "self_attn.q_proj.weight" in name or "wq.weight" in name:
-                    count_qk += 1
+                if ("q_proj.weight" in name or "wq.weight" in name):
+                    count_q += 1
+
                     dic["parameter_per_head"] = self.n_feature * self.n_feature // self.n_head
                     if (self.n_feature * self.n_feature % self.n_head) != 0:
                         raise ValueError("'n_feature * n_feature' is not a multiple of  n_head ")
 
-                if "self_attn.k_proj.weight" in name or "wk.weight" in name:
-                    count_qk += 1
-                    dic["parameter_per_head"] = self.n_feature * self.n_feature // self.n_kv_head
-                    if (self.n_feature * self.n_feature % self.n_kv_head) != 0:
-                        raise ValueError("'n_feature * n_feature' is not a multiple of  n_kv_head ")
+                if ("k_proj.weight" in name or "wk.weight" in name):
+                    count_k += 1
 
-                if "attn.attn.weight" in name or "attn.qkv.weight" in name:
-                    count_qk += 1
-                    dic["n_head"] = self.n_head
-                    dic["q_per_kv"] = self.n_head // self.n_kv_head
-                    if (self.n_head % self.n_kv_head) != 0:
-                        raise ValueError("'n_head' is not a multiple of n_kv_head ")
+                    dic["parameter_per_head"] = self.n_feature * self.n_feature // self.n_head
+                    if (self.n_feature * self.n_feature % self.n_head) != 0:
+                        raise ValueError("'n_feature * n_feature' is not a multiple of  n_head ")
 
                 optim_groups.append(dic)
 
+        print(
+            f'Adam-mini found {count_embd} embedding layers, {count_output} output layers, {count_q} Querys, {count_k} Keys.')
+
         if count_embd == 0:
-            print("=====>>> Warning: No embedding layer found. If you are training Transformers, please check the name of your embedding layer and manually add them to 'self.embd_blocks' of Adam-mini.")
+            # warning
+            print(
+                "=====>>> Warning by Adam-mini: No embedding layer found. If you are training Transformers, please check the name of your embedding layer and manually add them to 'self.embd_blocks' of Adam-mini.")
         if count_output == 0:
-            print("=====>>> Warning: No output layer found.  If you are training Transformers, please check the name of your output layer and manually add them to 'self.embd_blocks' of Adam-mini")
-        if count_qk == 0:
-            print("=====>>>  Warning: No Query or Key found.  If you are training Transformers, please check the name of your Query and Key in attention blocks and manually add them to 'self.qk_blocks' of Adam-mini")
+            # warning
+            print(
+                "=====>>> Warning by Adam-mini: No output layer found.  If you are training Transformers (without weight-tying), please check the name of your output layer and manually add them to 'self.embd_blocks' of Adam-mini. Please ignore this warning if you are using weight-tying.")
+        if count_q == 0:
+            # warning
+            print(
+                "=====>>>  Warning by Adam-mini: No Query found.  If you are training Transformers, please check the name of your Query in attention blocks and manually add them to 'self.qk_blocks' of Adam-mini")
 
-        if count_output + count_embd + count_qk == 0:
-            print("=====>>>  Warning: you are using default PyTorch partition for Adam-mini. It can cause training instability on large-scale Transformers.")
+        if count_k == 0:
+            # warning
+            print(
+                "=====>>>  Warning by Adam-mini: No Key found.  If you are training Transformers, please check the name of your Key in attention blocks and manually add them to 'self.qk_blocks' of Adam-mini")
 
-        self.embd_blocks = {"embed_tokens", "embed", "embd", "wte", "lm_head", "tok_embeddings", "output.weight"}
-        self.qk_blocks = {"k_proj.weight", "q_proj.weight", "wq.weight", "wk.weight", "q.weight", "k.weight"}
-        self.fused_attn_blocks = {"attn.attn.weight", "attn.qkv.weight"}
+        if count_output + count_embd + count_q + count_k == 0:
+            print(
+                "=====>>>  Warning by Adam-mini: you are using default PyTorch partition for Adam-mini. It can cause training instability on large-scale Transformers.")
+
+        # embd_blocks, including embd and output layers. Use normal adamW updates for these blocks
+        self.embd_blocks = {
+            "embed", "embd", "wte", "lm_head.weight", "output.weight"
+        }
+        # Query and Keys, will  assign lrs by heads
+        self.qk_blocks = {
+            "k_proj.weight", "q_proj.weight", "wq.weight", "wk.weight"
+        }
 
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
@@ -110,11 +139,7 @@ class Adam_mini(Optimizer):
 
         super(Adam_mini, self).__init__(optim_groups, defaults)
 
-    def step(self, closure=None):
-        loss = None
-        if closure is not None:
-            loss = closure()
-
+    def step(self):
         with torch.no_grad():
             for group in self.param_groups:
                 beta1 = group["beta1"]
@@ -126,7 +151,7 @@ class Adam_mini(Optimizer):
                 for p in group["params"]:
                     state = self.state[p]
 
-                    if any(block in name for block in self.embd_blocks):
+                    if any(block in name for block in self.embd_blocks):  # this is for embedding and output layer
                         if p.grad is None:
                             continue
                         if len(state) == 0:
@@ -151,24 +176,29 @@ class Adam_mini(Optimizer):
                         stepsize = lr / bias_correction_1
                         p.addcdiv_(state["m"], h, value=-stepsize)
 
-                    elif any(block in name for block in self.qk_blocks):
+                    elif any(block in name for block in self.qk_blocks):  # this is for query and key
+
                         if p.grad is None:
                             continue
 
                         dim = group["parameter_per_head"]
 
-                        if len(state) == 0:
+                        if (len(state) == 0):
                             state["m"] = torch.zeros_like(p.data).to(torch.float32)
+
                             state["m"] = state["m"].view(-1, dim)
-                            state["head"] = state["m"].shape[0]
+                            state["head"] = state['m'].shape[0]
                             state["iteration"] = 0
+
+                            # to reconcile with DTensor
                             state["vmean"] = torch.zeros_like(state["m"][0:state["head"], 0:1]).to(device)
 
                         grad = p.grad.data.to(torch.float32)
-                        head = state["head"]
+                        head = state['head']
                         grad = grad.view(head, dim)
 
-                        tmp_lr = torch.mean(grad * grad, dim=1).unsqueeze(1).to(device)
+                        tmp_lr = torch.mean(grad * grad, dim=1).unsqueeze(1).to(
+                            device)  # unsqueeze to size([head, 1]) to match the size of vmean
 
                         state["vmean"].mul_(beta2).add_(tmp_lr, alpha=1 - beta2)
                         v = state["vmean"]
@@ -197,85 +227,53 @@ class Adam_mini(Optimizer):
                         update.mul_(lr)
                         p.add_(-update)
 
-                    elif any(block in name for block in self.fused_attn_blocks):
-                        if p.grad is None:
-                            continue
-                        if len(state) == 0:
-                            state["m"] = torch.zeros_like(p.data).to(torch.float32)
-                            state["m"] = state["m"].view(group["n_head"], group["q_per_kv"] + 2, -1)
-                            state["iteration"] = 0
-                            state["vmean"] = torch.zeros(group["n_head"], group["q_per_kv"] + 2).to(device)
-
-                        grad = p.grad.data.to(torch.float32)
-                        grad = grad.view(group["n_head"], group["q_per_kv"] + 2, -1)
-
-                        tmp_lr = torch.mean(grad * grad, dim=2).to(device)
-                        state["vmean"].mul_(beta2).add_(tmp_lr, alpha=1 - beta2)
-                        v = state["vmean"]
-
-                        state["iteration"] += 1
-                        if group["weight_decay"] != 0:
-                            p.data.mul_(1 - lr * group["weight_decay"])
-
-                        state["m"].lerp_(grad, 1 - beta1)
-
-                        bias_correction_1 = 1 - beta1 ** state["iteration"]
-                        bias_correction_2 = 1 - beta2 ** state["iteration"]
-                        bias_correction_2_sqrt = math.sqrt(bias_correction_2)
-
-                        h = (v.sqrt() / bias_correction_2_sqrt).add_(eps)
-                        stepsize = ((1 / bias_correction_1) / h).view(group["n_head"], group["q_per_kv"] + 2, 1)
-
-                        update = state["m"] * (stepsize.to(state['m'].device))
-
-                        if p.dim() > 1:
-                            d0, d1 = p.size()
-                            update = update.view(d0, d1)
-                        else:
-                            update = update.view(-1)
-
-                        update.mul_(lr)
-                        p.add_(-update)
-
-                    else:
-                        if len(state) == 0:
+                    else:  # other blocks
+                        if (len(state) == 0):
                             dimension = torch.tensor(p.data.numel()).to(device).to(torch.float32)
                             reduced = False
-                            if self.world_size > 1 and self.model_sharding is True:
+                            if (self.world_size > 1) and (self.model_sharding is True):
                                 tensor_list = [torch.zeros_like(dimension) for _ in range(self.world_size)]
+
                                 dist.all_gather(tensor_list, dimension)
                                 s = 0
                                 dimension = 0
                                 for d in tensor_list:
-                                    if d > 0:
-                                        s += 1
-                                    dimension += d
-                                if s >= 2:
+                                    if (d > 0):
+                                        s = s + 1
+                                    dimension = dimension + d
+                                if (s >= 2):
                                     reduced = True
 
                             state["m"] = torch.zeros_like(p.data).to(torch.float32)
                             state["iteration"] = 0
                             state["reduced"] = reduced
+
+                            # actually this is just tensor(0.0), but this implementation is compatible with more general
+                            # types of tensor such as  DTensor in Titan
                             state["vmean"] = torch.zeros_like(torch.sum(p.data * p.data)).to(device)
                             state["dimension"] = dimension.item()
-
                         if p.grad is None:
+                            # actually this is just tensor(0.0), but this implementation is compatible with more general
+                            # types of tensor such as  DTensor in Titan
                             tmp_lr = torch.zeros_like(torch.sum(p.data * p.data)).to(device)
                         else:
                             grad = p.grad.data.to(torch.float32)
                             tmp_lr = torch.sum(grad * grad).to(device)
 
-                        if state["reduced"]:
+                        if (state["reduced"]):
                             if "device_mesh" in dir(tmp_lr):
+                                # when tmp_lr is a  DTensor in TorchTitan
                                 lr_local = tmp_lr.to_local()
                                 dist.all_reduce(lr_local, op=dist.ReduceOp.SUM)
                                 tmp_lr.redistribute(placements=[Replicate()])
                             else:
+                                # when tmp_lr is a  standard tensor
+                                # print(f"...... dist all reduce.......")
                                 dist.all_reduce(tmp_lr, op=dist.ReduceOp.SUM)
 
-                        if p.grad is None:
+                        if (p.grad is None):
                             continue
-                        tmp_lr = tmp_lr / state["dimension"]
+                        tmp_lr = tmp_lr / (state["dimension"])
                         tmp_lr = tmp_lr.to(grad.device)
 
                         if group["weight_decay"] != 0:
@@ -293,5 +291,3 @@ class Adam_mini(Optimizer):
                         update = state["m"] * (stepsize.to(state['m'].device))
                         update.mul_(lr)
                         p.add_(-update)
-
-        return loss
